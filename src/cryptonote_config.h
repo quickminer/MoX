a0bc9dad94740dd2fb2546e636454b3c1fcad79d
darylxp <darylxp@protonmail.com>
2018-07-22 04:07:41 -0700

    Fix decimal

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -62,9 +62,9 @@
 #define CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V5    300000 //size of block (bytes) after which reward for block calculated using block size - second change, from v5
 #define CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE          600
 
-#define CRYPTONOTE_DISPLAY_DECIMAL_POINT 12
+#define CRYPTONOTE_DISPLAY_DECIMAL_POINT 11
 // COIN - number of smallest units in one coin
-#define COIN ((uint64_t)1000000000000) // pow(10, 12)
+#define COIN ((uint64_t)100000000000)
 
 #define FEE_PER_KB_OLD                                  ((uint64_t)10000000000) // pow(10, 10)
 #define FEE_PER_KB                                      ((uint64_t)2000000000) // 2 * pow(10, 9)
